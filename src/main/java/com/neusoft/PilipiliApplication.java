2f6b4f49a55d8288a230792412850ff74f65f410
sup194 <18211367042@163.com>
2020-09-22 10:59:37 +0800

    pilipili项目初始化

---
+++ src/main/java/com/neusoft/PilipiliApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilipiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilipiliApplication.class, args);
+    }
+
+}
